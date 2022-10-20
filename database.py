9f62c6c79520bce670d3b90a723c617aca892ef8
Chris L <clinscott@oxy.edu>
2022-10-20 13:04:19 -0700

    Allows grabbing frames from videos + Updated DB for npy files
    Not my original code

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import numpy
+import ujson
 import json
 import os
 import h5py
@@ -7,22 +9,32 @@ class Database:
     def __init__(self, database_file_path: str) -> None:
         self.object_predictions_fp = os.path.join(
             database_file_path, 'objects.json')
-        self.predictions_fp = os.path.join(
-            database_file_path, 'predictions.json')
         self.object_predictions = self.load_json_data(
             self.object_predictions_fp)
-        self.predictions = self.load_json_data(self.predictions_fp)
+        
+        self.predictions_fp = os.path.join(database_file_path, 'predictions.npy')
+        self.prediction_image_paths_fp = os.path.join(database_file_path, 'prediction_image_paths.json')
+        self.prediction_image_paths = self.load_json_data(self.prediction_image_paths_fp)
+        self.predictions = self.load_npy_data(self.predictions_fp)
 
     def store_predictions(self, predictions, image_paths) -> None:
-        # Adds new predictions to already stored predictions, overriding stored values for new ones
+        # Adds new predictions to already stored predictions, or creates new file to store predictions
+        if numpy.size(self.predictions, axis=0) > 0:
+            self.predictions = numpy.append(self.predictions, predictions, axis=0)
+        else:
+            self.predictions = predictions
+        numpy.save(self.predictions_fp, self.predictions)
+        
         image_path_prediction_map = {}
-        for index, prediction in enumerate(predictions):
-            image_path_prediction_map[image_paths[index]] = prediction.tolist()
-        self.predictions.update(image_path_prediction_map)
-        with open(self.predictions_fp, 'w') as f:
-            f.write(json.dumps(self.predictions) + "\n")
+        max_index = len(self.prediction_image_paths)
+        for i in range(max_index, max_index + len(image_paths)):
+            image_path_prediction_map[image_paths[i - max_index]] = i
+        self.prediction_image_paths.update(image_path_prediction_map)
 
-    def store_object_data(self, image_paths, object_infos):
+        with open(self.prediction_image_paths_fp, 'w') as f:
+            f.write(json.dumps(self.prediction_image_paths) + "\n")
+
+    def store_object_data(self, object_infos, image_paths):
         image_path_object_data_map = {}
         for index, object_info in enumerate(object_infos):
             image_path_object_data_map[image_paths[index]] = {
@@ -33,12 +45,20 @@ class Database:
         self.object_predictions.update(image_path_object_data_map)
         with open(self.object_predictions_fp, 'w') as f:
             f.write(json.dumps(self.object_predictions) + "\n")
-
-    def load_json_data(self, file_path):
-        if not os.path.exists(file_path):
+        
+    def load_json_data(self, json_file_path):
+        if not os.path.exists(json_file_path):
             return {}
-        with open(file_path) as f:
+        with open(json_file_path) as f:
             try:
-                return json.load(f)
+                return ujson.load(f)
             except:
                 return {}
+    
+    def load_npy_data(self, npy_file_path):
+        if not os.path.exists(npy_file_path):
+            return numpy.empty((0, 0))
+        try:
+            return numpy.load(npy_file_path)
+        except:
+            return numpy.empty((0, 0))
