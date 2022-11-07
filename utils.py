6388b9e315ae6b09843bbd765d2205c59af75b60
Chris L <clinscott@oxy.edu>
2022-11-06 17:47:53 -0800

    Moved grab_images_and_paths

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import numpy as np
 
 # Constants
 DATABASE_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/predictions"
+EVAL_DB_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/eval/predictions"
 
 def get_and_resize_image(image_path, resize_shape):
     width, height, channels = resize_shape
@@ -20,6 +21,46 @@ def grab_all_image_paths(image_dir):
     for extension in extensions:
         image_paths.extend(glob.glob(image_dir + extension))
     return image_paths
+
+def grab_images_and_paths(self, image_dir: str, image_paths: list = [], num_images: int = None) -> tuple([list, int]):
+    """Grab either a specific number (if num_images is not None) or all the images from a
+    directory you specify. 
+    
+    Note: to ensure we don't get predictions from already-computed images, if the image path already
+    exists in the database, we will skip over it.
+
+    Args:
+        image_dir (str): The directory to grab images from.
+        image_paths (list): The already-predicted (from DB) image-paths (that we can skip over).
+        num_images (int): The number of images you want to grab. If None, we grab all images.
+
+    Raises:
+        ValueError: If the number of images grabbed is equal to 0, then we throw an Error, as it's
+        unlikely you want/expect to grab 0 (for gathering predictions for example).
+
+    Returns:
+        tuple(list, int): A tuple containing a list of image paths, and the number of images grabbed.
+    """
+    loaded_images = []
+    loaded_image_paths = []
+    curr_image_paths = grab_all_image_paths(image_dir)
+    curr_index, num_loaded_images = 0, 0
+    while (not num_images or num_loaded_images < num_images) and curr_index < len(curr_image_paths):
+        curr_image_path = curr_image_paths[curr_index]
+        if curr_image_path not in image_paths:
+            try:
+                curr_image = get_and_resize_image(curr_image_path, self.model.input_shape[1:])
+                loaded_images.append(curr_image)
+                loaded_image_paths.append(curr_image_path)
+                num_loaded_images += 1
+            except Exception as e:
+                pass
+        curr_index += 1
+    if num_loaded_images == 0:
+        raise ValueError(f"No images could be found in {image_dir}, or all images from {image_dir} have been grabbed.")
+    loaded_images = np.concatenate(loaded_images, axis=0)
+    return loaded_image_paths, loaded_images
+
 
 # Video Utility Functions
 
