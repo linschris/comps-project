1a578ed9bf9c3d09f8655d54f711b316bc579c27
Chris L <clinscott@oxy.edu>
2022-11-10 16:49:29 -0800

    Removed unused video utility functions

--- utils.py
+++ utils.py
@@ -5,8 +5,19 @@ import numpy as np
 # Constants
 DATABASE_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/predictions"
 EVAL_DB_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/eval/predictions"
+DOG_DB_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/eval/dog-predictions"
+CAT_DB_PATH = "/Users/lchris/Desktop/Coding/schoolprojects/comp490/COMPS/data/eval/cat-predictions"
 
-def get_and_resize_image(image_path, resize_shape):
+def get_and_resize_image(image_path: str, resize_shape: tuple) -> np.ndarray:
+    """Load and resize a image to match a input shape, normally a model input shape.
+
+    Args:
+        image_path (str): Path to image to be loaded and resized.
+        resize_shape (Tuple): Tuple of size n dimensions denoting each dimensions size.
+
+    Returns:
+        np.ndarray: Image as a resized NDArray, the same shape as the resize shape
+    """
     width, height, channels = resize_shape
     img = cv2.imread(image_path)
     resized_img = cv2.resize(img, (width, height))
@@ -15,19 +26,27 @@ def get_and_resize_image(image_path, resize_shape):
     )
     return reshaped_img
 
-def grab_all_image_paths(image_dir):
+def grab_all_image_paths(image_dir: str) -> list[str]:
+    """Grab all the image paths of images with extensions (jpg, webp, and png) from a given directory.
+
+    Args:
+        image_dir (str): Directory to pull images from.
+
+    Returns:
+        list[str]: List of image paths as strings.
+    """
     image_paths = []
     extensions = ('/*.jpg', '/*.webp', '/*.png')
     for extension in extensions:
         image_paths.extend(glob.glob(image_dir + extension))
     return image_paths
 
-def grab_images_and_paths(self, image_dir: str, image_paths: list = [], num_images: int = None) -> tuple([list, int]):
+def grab_images_and_paths(image_dir: str, image_paths: list = [], num_images: int = None) -> tuple([list[str], list[np.ndarray]]):
     """Grab either a specific number (if num_images is not None) or all the images from a
     directory you specify. 
     
     Note: to ensure we don't get predictions from already-computed images, if the image path already
-    exists in the database, we will skip over it.
+    exists in the "database", we will skip over it.
 
     Args:
         image_dir (str): The directory to grab images from.
@@ -39,7 +58,7 @@ def grab_images_and_paths(self, image_dir: str, image_paths: list = [], num_imag
         unlikely you want/expect to grab 0 (for gathering predictions for example).
 
     Returns:
-        tuple(list, int): A tuple containing a list of image paths, and the number of images grabbed.
+        tuple(list[str], list[np.ndarray]): A tuple containing a list of image paths as strings, and a list of loaded images (as ndarrays).
     """
     loaded_images = []
     loaded_image_paths = []
@@ -49,11 +68,12 @@ def grab_images_and_paths(self, image_dir: str, image_paths: list = [], num_imag
         curr_image_path = curr_image_paths[curr_index]
         if curr_image_path not in image_paths:
             try:
-                curr_image = get_and_resize_image(curr_image_path, self.model.input_shape[1:])
+                curr_image = get_and_resize_image(curr_image_path, (299, 299, 3))
                 loaded_images.append(curr_image)
                 loaded_image_paths.append(curr_image_path)
                 num_loaded_images += 1
             except Exception as e:
+                # If we run into an error, just don't store and grab other images
                 pass
         curr_index += 1
     if num_loaded_images == 0:
@@ -85,38 +105,3 @@ def get_yt_embed_url(image_path: str) -> tuple([str, int]):
         curr_time_str = int(image_path[-9:-4])
         return video_embed_url, curr_time_str
     return video_embed_url, None
-
-def get_youtube_embed_url(image_path):
-    curr_str = (image_path.split("/")[-1]).split("_frame")
-    video_id = curr_str[0]
-    if len(curr_str) == 2:  # We're looking at a frame
-        time_str = int(curr_str[1][1:6])
-        return f'https://www.youtube.com/embed/{video_id}?start={time_str}'
-    elif "." in video_id:  # We're looking at a thumbnail
-        id_without_ext = video_id.split('.')[0]
-        return f'https://www.youtube.com/embed/{id_without_ext}'
-    else:
-        return f'https://www.youtube.com/embed/{video_id}'
-
-def get_youtube_time_str(time_str):
-    num_seconds = int(time_str)
-    num_hours = int(num_seconds / 3600)
-    num_minutes = int(num_seconds / 60)
-    num_seconds -= (num_hours * 3600 + num_minutes * 60)
-    curr_str = ""
-    if num_hours > 0:
-        if num_hours < 10:
-            curr_str += f'0{num_hours}h'
-        else:
-            curr_str += f'{num_hours}h'
-    if num_minutes > 0:
-        if num_minutes < 10:
-            curr_str += f'0{num_minutes}m'
-        else:
-            curr_str += f'{num_minutes}h'
-    if num_seconds > 0:
-        if num_seconds < 10:
-            curr_str += f'0{num_seconds}s'
-        else:
-            curr_str += f'{num_seconds}s'
-    return curr_str
